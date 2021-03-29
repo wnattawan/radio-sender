677d22a24356a8ab5a20ab94a590438f2781e151
wnattawan <wnattawan@gmail.com>
2021-03-29 23:34:02 +0200

    Add navigation controls

--- main.ts
+++ main.ts
@@ -1,7 +1,37 @@
 GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Up, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
-    GAME_ZIP64.runMotor(100)
     radio.sendString("up")
-    basic.showString("Sent")
+    basic.showArrow(ArrowNames.North)
     basic.pause(200)
 })
+
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Down, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {
+    radio.sendString("down")
+    basic.showArrow(ArrowNames.South)
+    basic.pause(200)
+})
+
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Left, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {    
+    radio.sendString("left")
+    basic.showArrow(ArrowNames.West)
+    basic.pause(200)
+})
+
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Right, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {    
+    radio.sendString("right")
+    basic.showArrow(ArrowNames.East)
+    basic.pause(200)
+})
+
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Fire1, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {    
+    radio.sendString("happy")
+    basic.showIcon(IconNames.Happy)
+    basic.pause(200)
+})
+
+GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Fire2, GAME_ZIP64.ZIP64ButtonEvents.Down, function () {    
+    radio.sendString("heart")
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+})
+
 radio.setGroup(1)
